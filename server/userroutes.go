ec008719409b437bfe075c51ea1f0342751dbda3
mateen <mateenbagheri@gmail.com>
2022-07-28 10:02:44 +0430

    server routes initialized. to be updated.

---
+++ server/userroutes.go
@@ -0,0 +1,13 @@
+package server
+
+import "github.com/gin-gonic/gin"
+
+func UserRoute(router *gin.Engine) {
+	user := router.Group("/user")
+	{
+		user.GET("")
+		user.POST("")
+		user.PUT("")
+		user.DELETE("")
+	}
+}
