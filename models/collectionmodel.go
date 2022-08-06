882a7f02cf6d09a2b6994dae6070f9ff8620e3e2
mateen <mateenbagheri@gmail.com>
2022-08-06 14:04:30 +0430

    added post/put functionallity for collections.TODO:Error handling, testing

--- models/collectionmodel.go
+++ models/collectionmodel.go
@@ -2,6 +2,6 @@ package models
 
 type Collection struct {
 	CollectionID   int    `json:"CollectionID"`
-	UserID         int    `json:"UserID"`
-	CollectionName string `json:"CollectionName" validate:"required"`
+	UserID         int    `json:"UserID" binding:"required"`
+	CollectionName string `json:"CollectionName" binding:"required"`
 }
